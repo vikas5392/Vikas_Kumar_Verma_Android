ed70fa04b9180f10d05659d76dece3fbb7816530
vikas_kumar_verma <vikasverma5392@gmail.com>
2021-08-04 16:07:50 +0530

    Image scaled in custom view for different screen size

--- Assignment-18-image in custom view/imageincustomview/app/src/main/java/com/training/imageincustomview/customview/CustomViewImage.java
+++ Assignment-18-image in custom view/imageincustomview/app/src/main/java/com/training/imageincustomview/customview/CustomViewImage.java
@@ -4,15 +4,25 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.View;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 
 import com.training.imageincustomview.R;
+
+import java.io.ByteArrayInputStream;
+
 public class CustomViewImage extends View {
     private Bitmap imageBitMap;
+    Rect srcRect, dstRect;
+    int bitmapWidth;
+    int bitmapHeight;
+
     public CustomViewImage(Context context) {
         super(context);
         init(null);
@@ -37,10 +47,16 @@ public void init(AttributeSet set){
 
     imageBitMap=BitmapFactory.decodeResource(getResources(), R.drawable.image);
 }
-
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
-canvas.drawBitmap(imageBitMap,20,20,null);
+        bitmapWidth = imageBitMap.getWidth();
+        bitmapHeight = imageBitMap.getHeight();
+
+        srcRect = new Rect(0, 0, bitmapWidth, bitmapHeight);//size of bitmap
+
+        dstRect = new Rect(0,0,getWidth(),getHeight());//size of the custom view
+
+        canvas.drawBitmap(imageBitMap, srcRect, dstRect, null);//scale bitmap from src to dst
     }
 }
