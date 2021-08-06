e5bf0c6675b260d2d40a425080391fb83ba57dc2
vikas_kumar_verma <vikasverma5392@gmail.com>
2021-08-06 16:24:13 +0530

    updates

--- Assignment-15-app with 3 option menu/OptionMenu/app/src/main/java/com/training/optionmenu/MainActivity.java
+++ Assignment-15-app with 3 option menu/OptionMenu/app/src/main/java/com/training/optionmenu/MainActivity.java
@@ -8,18 +8,41 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.zip.Inflater;
 
 public class MainActivity extends AppCompatActivity {
+
+    ListView list_V;
+    ArrayList<String> fruitList;
+    ArrayList<String> movieList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+        list_V=findViewById(R.id.listView);
 
+        fruitList=new ArrayList<>();
+        fruitList.add( "FRUITS");
+        fruitList.add("banana");
+        fruitList.add("apple");
+        fruitList.add("orange");
+        fruitList.add("pineapple");
+
+       movieList=new ArrayList<>();
+        movieList.add("MOVIES");
+        movieList.add("Inception");
+        movieList.add("Harry Potter");
+        movieList.add("Avatar");
+        movieList.add("Suicide Squad");
+
+    }
+   //finally convert the xml file ie menudemo to a view by inflater
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
        MenuInflater inflater=getMenuInflater();
@@ -30,25 +53,20 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         switch(item.getItemId()) {
-            case R.id.apple:
-                Toast.makeText(this, "Fruits:Apple", Toast.LENGTH_SHORT).show();
+            case R.id.fruits:
+                ArrayAdapter<String> F_adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,fruitList);
+               list_V.setAdapter(F_adapter);
+
                 return true;
-            case R.id.banana:
-                Toast.makeText(this, "Fruits:Banana", Toast.LENGTH_SHORT).show();
+
+
+            case R.id.movies:
+
+                ArrayAdapter<String> m_adapter=new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,movieList);
+                list_V.setAdapter(m_adapter);
                 return true;
-            case R.id.mango:
-                Toast.makeText(this, "Fruits:Mango", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.inception:
-                Toast.makeText(this, "Movies:Inception", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.justiceLeague:
-                Toast.makeText(this, "Movie:Justice League", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.endGame:
-                Toast.makeText(this, "Movie:End Game", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.close:
+
+                case R.id.close:
                 finish();
                 System.exit(0);
         }
