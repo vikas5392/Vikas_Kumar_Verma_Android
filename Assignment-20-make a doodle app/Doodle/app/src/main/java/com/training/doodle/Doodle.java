c65098d5bba8af622cba2eab2f917749d7f7cb41
vikas_kumar_verma <vikasverma5392@gmail.com>
2021-08-06 14:37:54 +0530

    Doodle App

---
+++ Assignment-20-make a doodle app/Doodle/app/src/main/java/com/training/doodle/Doodle.java
@@ -0,0 +1,106 @@
+package com.training.doodle;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.drawable.Drawable;
+import android.media.ThumbnailUtils;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+
+
+public class Doodle extends View {
+
+    Bitmap m_bitmap;
+    Canvas m_canvas;
+    Paint m_paint;
+    ViewGroup.LayoutParams m_params;
+    Path m_path;
+    Paint mBitmapPaint;
+
+    public Doodle(Context context,AttributeSet attrs) {
+        super(context,attrs);
+        init(null);
+        m_paint.setAntiAlias(true);
+        m_paint.setColor(Color.CYAN);
+        m_paint.setStyle(Paint.Style.STROKE);
+        m_paint.setStrokeJoin(Paint.Join.ROUND);
+        m_paint.setStrokeWidth(8f);
+        mBitmapPaint = new Paint(Paint.DITHER_FLAG);
+    }
+
+    public Doodle(Context context) {
+        super(context);
+      init(null);
+    }
+
+    private void init(AttributeSet set){
+        m_paint=new Paint();
+        m_path=new Path();
+        m_params=new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+    }
+
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+
+        m_bitmap = Bitmap.createBitmap(w, h, Bitmap.Config.ARGB_8888);
+        m_canvas = new Canvas(m_bitmap);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        float pointX=event.getX();
+        float pointY=event.getY();
+        switch(event.getAction()){
+            case MotionEvent.ACTION_DOWN:m_path.moveTo(pointX,pointY);
+            return true;
+            case MotionEvent.ACTION_MOVE:m_path.lineTo(pointX,pointY);
+            break;
+            default:
+                return false;
+        }
+        postInvalidate();
+        return false;
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawBitmap( m_bitmap, 0, 0, mBitmapPaint);
+        canvas.drawPath( m_path,  m_paint);
+
+    }
+    public Bitmap loadBitmapFromView(View v) {
+        DisplayMetrics dm = getResources().getDisplayMetrics();
+        v.measure(View.MeasureSpec.makeMeasureSpec(dm.widthPixels,
+                View.MeasureSpec.EXACTLY),
+                View.MeasureSpec.makeMeasureSpec(dm.heightPixels,
+                        View.MeasureSpec.EXACTLY));
+        v.layout(0, 0, v.getMeasuredWidth(), v.getMeasuredHeight());
+        Bitmap returnedBitmap =
+                Bitmap.createBitmap(v.getMeasuredWidth(),
+                        v.getMeasuredHeight(), Bitmap.Config.ARGB_8888);
+        Canvas c = new Canvas(returnedBitmap);
+        v.draw(c);
+
+        return returnedBitmap;
+    }
+}
+
---
+++ Assignment-20-make a doodle app/Doodle/app/src/main/java/com/training/doodle/MainActivity.java
@@ -0,0 +1,101 @@
+package com.training.doodle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
+
+import android.annotation.SuppressLint;
+import android.content.ContentValues;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.util.UUID;
+
+public class MainActivity extends AppCompatActivity {
+    Doodle doodleView;
+    ConstraintLayout constraint_l;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        constraint_l=findViewById(R.id.constraint);
+        doodleView=new Doodle(this);
+    }
+    public void save(View view) {
+
+        saveImage(doodleView.loadBitmapFromView(constraint_l));
+        Toast.makeText(this, "saved", Toast.LENGTH_SHORT).show();
+    }
+    void saveImage(Bitmap bitmap) {
+        if (android.os.Build.VERSION.SDK_INT >= 29) {
+            ContentValues values = contentValues();
+            values.put(MediaStore.Images.Media.RELATIVE_PATH, "Pictures/" + getString(R.string.app_name));
+            values.put(MediaStore.Images.Media.IS_PENDING, true);
+
+            Uri uri = this.getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            if (uri != null) {
+                try {
+                    saveImageToStream(bitmap, this.getContentResolver().openOutputStream(uri));
+                    values.put(MediaStore.Images.Media.IS_PENDING, false);
+                    this.getContentResolver().update(uri, values, null, null);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        } else {
+            File directory = new File(Environment.getExternalStorageDirectory().toString() + '/' + getString(R.string.app_name));
+
+            if (!directory.exists()) {
+                directory.mkdirs();
+            }
+            String fileName = System.currentTimeMillis() + ".png";
+            File file = new File(directory, fileName);
+            try {
+                saveImageToStream(bitmap, new FileOutputStream(file));
+                ContentValues values = new ContentValues();
+                values.put(MediaStore.Images.Media.DATA, file.getAbsolutePath());
+                getApplicationContext().getContentResolver().insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
+            } catch (FileNotFoundException e) {
+                e.printStackTrace();
+            }
+
+        }
+    }
+
+    private ContentValues contentValues() {
+        ContentValues values = new ContentValues();
+        values.put(MediaStore.Images.Media.MIME_TYPE, "image/png");
+        values.put(MediaStore.Images.Media.DATE_ADDED, System.currentTimeMillis() / 1000);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            values.put(MediaStore.Images.Media.DATE_TAKEN, System.currentTimeMillis());
+        }
+        return values;
+    }
+
+    private void saveImageToStream(Bitmap bitmap, OutputStream outputStream) {
+        if (outputStream != null) {
+            try {
+                bitmap.compress(Bitmap.CompressFormat.JPEG, 100, outputStream);
+                outputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    }
+
